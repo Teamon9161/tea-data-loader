1ee2317189787c3e203c6b9f5f681ebbbc874715
Teamon9161 <1127972945@qq.com>
2024-07-24 17:31:28 +0800

    add simple test

--- src/lib.rs
+++ src/lib.rs
@@ -15,11 +15,16 @@ mod tests {
     pub fn test_base() -> Result<()> {
         let dl = DataLoader::new("future")
             .with_symbols(["A", "CU", "RB"])
-            .with_start("2020-01-01 00:00:00")
+            .with_start("2020-01-01")
             .kline("min", None, None, true)?
             .with_noadj(None, false, true)?
             .collect(true)?;
         dbg!("{:#?}", &dl["A"]);
+        let dl = DataLoader::new("future")
+            .kline("daily", None, None, true)?
+            .with_noadj(None, false, true)?
+            .collect(true)?;
+        dbg!("{:#?}", &dl["AG"]);
         Ok(())
     }
 }
